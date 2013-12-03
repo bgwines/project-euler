0643e144650f6340bc1acb30ada0c3da2546ec6a
Brett Wines <bgwines@gmail.com>
2013-12-03 12:29:16 -0800

    Cleaned up 9.

--- solved/problem9.hs
+++ solved/problem9.hs
@@ -9,6 +9,26 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 -}
 
-triplets = [(a,b,c) | a<-[1..998], b<-[(a+1)..999], c<-[(b+1)..1000], a+b+c==1000]
+import Data.List
 
-pythag_triplets = filter (\(a,b,c) -> a*a + b*b == c*c) triplets
+triplets :: [(Integer, Integer, Integer)]
+triplets = [(a,b,c) | 
+		a<-[1..998], 
+		b<-[(a+1)..999], 
+		c<-[(b+1)..1000], 
+		a + b + c == 1000
+	]
+
+pythag_triplet :: (Integer, Integer, Integer)
+pythag_triplet = (\(Just x) -> x)
+	$ find is_pythag_triplet triplets
+		where is_pythag_triplet (a,b,c) = a^2 + b^2 == c^2
+
+pythag_triplet_prod :: Integer
+pythag_triplet_prod = 
+	(fst3 pythag_triplet) *
+	(snd3 pythag_triplet) *
+	(trd3 pythag_triplet)
+
+main = do
+	(putStrLn . show) pythag_triplet_prod
