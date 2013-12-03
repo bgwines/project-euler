4c64eccecdf37367f2ea90555d1db7f19b718cf0
Brett Wines <bgwines@gmail.com>
2013-12-03 12:58:49 -0800

    Cleaned up 1.

--- solved/problem1.hs
+++ solved/problem1.hs
@@ -4,10 +4,11 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 -}
 
---mults_under_n :: (Integer a) => b -> [a]
-mults_under_param n =
-	filter
-		(\x -> or (map (\y -> x `mod` y == 0) [3,5]))
-		[1..n-1]
+get_mults_of_3_and_3_under :: Integer -> [Integer]
+get_mults_of_3_and_3_under n = filter divides_3_and_5 [1..n-1]
+	where divides_3_and_5 m = 
+		(m `mod` 3 == 0) &&
+		(m `mod` 5 == 0)
 
-s = sum $ mults_under_param 1000
+main = do
+	(putStrLn . show) $ sum $ get_mults_of_3_and_3_under 1000
