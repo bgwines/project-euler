b53a38d685d609da186152da6ed62cbcaf8c67e5
Brett Wines <bgwines@gmail.com>
2013-12-03 12:41:30 -0800

    Cleaned up 5.

--- solved/problem5.hs
+++ solved/problem5.hs
@@ -5,24 +5,23 @@ What is the smallest positive number that is evenly divisible by all of the numb
 -}
 
 import Data.List
+import Prime
 
-is_prime_rec :: (Num a, Integral a) => a -> a -> Bool
-is_prime_rec n k
-	| n == 1 = False
-	| k < 2 = True
-	| ((n `mod` k) == 0) = False
-	| otherwise = is_prime_rec n (k-1)
-
-is_prime :: (Num a, Integral a) => a -> Bool
-is_prime n = is_prime_rec n (n-1)
-
-divisible_for_all_in_list :: (Num a, Integral a) => [a] -> a -> Bool
+divisible_for_all_in_list :: [Integer] -> Integer -> Bool
 divisible_for_all_in_list l n =
 	and [n `mod` k == 0 | k <- l]
 
 -- 1,2,3,4,5,8,9,10 all have multiples in l
+l :: [Integer]
 l = [20,19,18,17,16,15,14,13,12,11]
 
+product_of_primes :: Integer
 product_of_primes = product $ filter is_prime l
-sought = find (divisible_for_all_in_list l)
-	$ map (\x -> x * product_of_primes) [1..]
+
+divider :: Integer
+divider = (\(Just x) -> x) $
+	find (divisible_for_all_in_list l) products_of_primes
+		where products_of_primes = map (* product_of_primes) [1..]
+
+main = do
+	(putStrLn . show) $  divider
