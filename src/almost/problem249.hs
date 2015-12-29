6b08cfe7753c63e984b8d0268cef71600667fc33
Brett Wines <brettwines@gmail.com>
2015-12-29 14:20:38 -0800

    working but too slow 249

--- src/almost/problem249.hs
+++ src/almost/problem249.hs
@@ -7,6 +7,9 @@ Find the number of subsets of S, the sum of whose elements is a prime number. En
 
 import qualified Data.Numbers.Primes as P (primes, isPrime)
 import qualified Data.MemoCombinators as Memo
+import Zora.List (powerset)
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 {-
 1548136 -- desired sum of S
@@ -24,30 +27,8 @@ import qualified Data.MemoCombinators as Memo
    2 3   5   7...4999 -- max usable prime (sUB)
 -}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 sUB :: Integer
-sUB = 7 --5000
+sUB = 20 --5000
 
 s :: [Integer]
 s = takeWhile (< sUB) P.primes
@@ -58,21 +39,26 @@ numFormations 0 _ = 1 -- formation is the empty set
 numFormations 1 _ = 0 -- no formations possible
 numFormations n maxPrime
     | (n < 0) = 0 -- can't sum to a negative number with only positive numbers
-    | otherwise = (Memo.memo2 Memo.integral Memo.integral numFormations') n maxPrime
+    | otherwise = f n maxPrime
+    where
+        f = Memo.memo2 Memo.integral Memo.integral numFormations'
 
 -- | Number of ways to form `n` as a summation of primes less than `maxPrime`
 numFormations' :: Integer -> Integer -> Integer
 numFormations' n maxPrime
     = sum
     . map (\p -> numFormations (n - p) p)
-    $ takeWhile (< maxPrime) P.primes
+    $ takeWhile (< maxPrime) s
 
 main :: IO ()
 main = do
-    putStrLn $ "S = filter prime [2.." ++ show sUB ++ "]"
+    putStrLn $ "S = takeWhile (< " ++ show sUB ++ ") P.primes"
     putStr "Number of subsets of S that sum to a prime number: "
+    let ssum = sum s
     print
         . sum -- TODO: fold with truncating +
         . map (\n -> numFormations n (n + 1)) -- `+ 1` because of noninclusiveness
-        . takeWhile (<= (sum s)) -- `sum s` is for the subset that is `S`; the maximal subset
+        . takeWhile (<= ssum) -- `sum s` is for the subset that is `S`; the maximal subset
         $ P.primes
+    putStr "Brute force: "
+    print $ length . filter P.isPrime . map sum . powerset $ s
---
+++ src/almost/problem249.py
@@ -0,0 +1,75 @@
+
+import pdb
+import pyprimes
+
+'''
+Let S = {2, 3, 5, ..., 4999} be the set of prime numbers less than 5000.
+
+Find the number of subsets of S, the sum of whose elements is a prime number. Enter the rightmost 16 digits as your answer.
+'''
+
+'''
+1548136 # desired sum of S
+...
+8 |
+7 |x x   .   .
+6 |x x   x   x
+5 |x .   .   .
+4 |x x   x   x
+3 |x .   .   .
+2 |. .   .   .
+1 |
+0 |
+  ######
+   2 3   5   7...4999 # max usable prime (sUB)
+'''
+
+def takeWhile(p, xs):
+    result = []
+    for x in xs:
+        if not p(x):
+            break
+        result.append(x)
+    return result
+
+sUB = 300 #5000
+
+s = takeWhile(lambda p: p < sUB, pyprimes.primes())
+
+# | Number of ways to form `n` as a summation of primes less than `maxPrime`
+memo = dict()
+# @profile
+def numFormations(n, maxPrime):
+    if (n == 0):
+        return 1 # formation is the empty set
+
+    if (n, maxPrime) in memo:
+        return memo[(n, maxPrime)]
+
+    result = 0
+    for p in s:
+        if p >= maxPrime or p > n:
+            break
+        result += numFormations(n - p, p)
+        result %= 10000000000000000
+    memo[(n, maxPrime)] = result
+    return result
+
+def main():
+    print "S = takeWhile (< ", sUB, ") P.primes"
+    print "Number of subsets of S that sum to a prime number: "
+
+    result = 0
+    ssum = sum(s)
+    for p in pyprimes.primes():
+        # `sum s` is for the subset that is `S`; the maximal subset
+        if not p <= ssum:
+            break
+
+        # `+ 1` because of noninclusiveness
+        result += numFormations(p, p + 1)
+        result %= 10000000000000000
+    print result
+
+if __name__ == '__main__':
+    main()
