06c0dd8e4b2de2909bb8f48e9b02d181de7a19b0
Brett Wines <brettwines@gmail.com>
2016-02-10 21:10:10 -0800

    Clean up problem44

--- src/solved/problem44.hs
+++ src/solved/problem44.hs
@@ -1,41 +1,44 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import qualified Data.Ord as Ord
-import qualified Data.Set as Set
 import qualified Data.List as List
-import qualified Zora.List as ZList
 import qualified Zora.Math as ZMath
 
-qf :: (Floating a) => (a -> a -> a) -> Integer -> a
-qf op m = (1/6) * (op (fromInteger 1) (sqrt $ fromInteger (24 * m + 1)))
-
-quad_formula :: (Floating a) => Integer -> [a]
-quad_formula m = [qf (-) m, qf (+) m]
+quad_formula :: forall a. (Floating a) => Integer -> [a]
+quad_formula m = [qf (-), qf (+)]
+    where
+        qf :: (a -> a -> a) -> a
+        qf op = (1 / 6) * (op (fromInteger 1) (sqrt $ fromInteger (24 * m + 1)))
 
 is_pentagonal :: Integer -> Bool
 is_pentagonal = any (\m -> (ZMath.is_int m) && (m >= 0)) . quad_formula
 
 diff_and_sum_pentagonal :: (Integer, Integer) -> Bool
 diff_and_sum_pentagonal (a, b) = and
-	[ is_pentagonal (b - a)
-	, is_pentagonal (b + a) ]
+    [ is_pentagonal (b - a)
+    , is_pentagonal (b + a) ]
 
-pentagonals :: [Integer] 
-pentagonals = map pent [1..5000]--10000]
-	where
-		pent :: Integer -> Integer
-		pent n = div (n * (3*n - 1)) 2
+pentagonals :: [Integer]
+pentagonals = map pent [1..5000]
+    where
+        pent :: Integer -> Integer
+        pent n = div (n * ((3 * n) - 1)) 2
 
 valid_differences :: [(Integer, Integer)]
-valid_differences = filter 
-	diff_and_sum_pentagonal
-	[ (pentagonals !! i, b)
-		| i <- [0..(length pentagonals)]
-		, b <- (drop (i+1) pentagonals)]
-
-abs_diffs :: [((Integer, Integer), Integer)]
-abs_diffs = map (\(a, b) -> ((a, b), abs $ a - b)) valid_differences
+valid_differences
+    = filter diff_and_sum_pentagonal
+    . concatMap f . List.tails -- pairs of elems where fst is < snd
+    $ pentagonals
+    where
+        f :: [Integer] -> [(Integer, Integer)]
+        f [] = []
+        f (x:[]) = []
+        f (x:xs) = map (x, ) xs
 
 main :: IO ()
 main = do
-	putStrLn . show $ List.minimumBy (Ord.comparing snd) abs_diffs
-
+    print $ minimum . map d $ valid_differences
+    where
+        d :: (Integer, Integer) -> Integer
+        d = uncurry (flip (-))
